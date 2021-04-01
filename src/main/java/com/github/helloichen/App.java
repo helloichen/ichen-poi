438c5f1fbf9cc88f11aea26dafb898e4d8f8f8ef
chenwp <helloichen@163.com>
2021-04-01 23:27:56 +0800

    pom

--- src/main/java/com/github/helloichen/App.java
+++ src/main/java/com/github/helloichen/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! Hello iChen");
     }
+
+    public static Long now(){
+        return System.currentTimeMillis();
+    }
 }
